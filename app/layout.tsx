6fdb2cc0accad5dc8c3b0c2dbf117712489a6a2d
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-08-08 17:00:22 +0900

    navigation, DataFetch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import styles from '@/app/layout.module.css';
+import Navigation from '@/components/navigation';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${styles.main}`}>
+        <Navigation />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/navigation.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Link from 'next/link';
+import styles from './navigation.module.css';
+import { usePathname } from 'next/navigation';
+
+export default function Navigation() {
+  const path = usePathname();
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.box}>
+        {path === '/' ? (
+          <Link href="/" className={styles.link}>
+            Billionaires-Wiki
+          </Link>
+        ) : (
+          <Link href="/">Home</Link>
+        )}
+      </div>
+    </nav>
+  );
+}
